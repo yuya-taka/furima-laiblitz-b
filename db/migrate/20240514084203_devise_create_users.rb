a15667303881eb5350f3c5d6da06aa0fab2c1e6a
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-14 17:57:14 +0900

    カラム追記してみた

--- db/migrate/20240514084203_devise_create_users.rb
+++ db/migrate/20240514084203_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :email, null: false, unique: true
+      t.string :encrypted_password, null: false
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.date :birthday, null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
