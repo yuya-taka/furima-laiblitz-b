38033f0f93e45f7df1ca1fd6e0c2439ddf460a67
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-14 19:20:29 +0900

    ユーザー登録機能が一部できました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+  end
+
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- db/migrate/20240514084203_devise_create_users.rb
+++ db/migrate/20240514084203_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :nickname#, null: false
       t.string :email#, null: false, unique: true
-      t.string :encrypted_password#, null: false
+      t.string :password#, null: false
       t.string :family_name#, null: false
       t.string :first_name#, null: false
       t.string :family_name_kana#, null: false
