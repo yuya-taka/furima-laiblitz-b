855f4fb625a4ad43a558edb57743ef3f45bdf8e2
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-16 18:18:22 +0900

    商品画像以外のバリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,4 +15,14 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_date, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_date_id, presence: true
+  validates :prefecture_id, presence: true
+
 end
