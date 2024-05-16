34cbb6794c2a7a2e85c2a57a4ce13dd7ccf08b04
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-16 18:18:21 +0900

    historyテーブルにカラムを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_16_065259) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_16_090952) do
+  create_table "histories", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_histories_on_item_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
+  end
+
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
@@ -44,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_16_065259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "histories", "items"
+  add_foreign_key "histories", "users"
   add_foreign_key "items", "users"
 end
