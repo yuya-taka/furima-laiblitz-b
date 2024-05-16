8755b48005b5771bc781cd2c86a5b73c64bddf41
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-16 18:13:26 +0900

    配送についてのプルダウンの作成

---
+++ app/models/delivery_date.rb
@@ -0,0 +1,13 @@
+class Delivery_date < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },   # ←こちらに注目しましょう
+    { id: 2, name: '1~2日で配送' },
+    { id: 3, name: '2~3日で配送' },
+    { id: 3, name: '4~7日で配送' }
+  ]
+
+ 
+   include ActiveHash::Associations
+   has_many :items
+  
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,15 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_date
+  
   validates :title, :text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_date, numericality: { other_than: 1 , message: "can't be blank"}
 end
