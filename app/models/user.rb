fc00c8dbc47ca53f4cff7849bf56c91bc976f68c
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-16 11:34:59 +0900

    バリデーションの修正と不要な文の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,28 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         validates :nickname, presence: true
-        #  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効なメールアドレスではありません" }
 
+  validates :nickname, presence: true
+  #  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効なメールアドレスではありません" }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
+  validates :password_confirmation, presence: true
+  validate :password_confirmation_match
 
-         validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
-
-
-         validates :password_confirmation, presence: true
-         validate :password_confirmation_match
-       
-         private
-       
-         # パスワードとパスワード（確認）の一致を確認するカスタムバリデーション
-         def password_confirmation_match
-           errors.add(:password_confirmation, "とパスワードが一致しません") unless password == password_confirmation
-         end
-
-        #  validates :family_name, :first_name, presence: true
-         validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角（漢字・ひらがな・カタカナ）での入力が必須です" }
-         validates :first_name_kana, :family_name_kana, presence: true
-        #  validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）での入力が必須です" }
-         validates :birthday, presence: true
+  private
+  # パスワードとパスワード（確認）の一致を確認するカスタムバリデーション
+  def password_confirmation_match
+    errors.add(:password_confirmation, "とパスワードが一致しません") unless password == password_confirmation
+  end
+  validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）での入力が必須です" }
+  validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角（カタカナ）での入力が必須です" }
+  validates :birthday, presence: true
 
 end
