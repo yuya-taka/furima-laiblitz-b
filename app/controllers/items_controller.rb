d3c09d695149ea266f1031ffd5f74eb57ee52fc2
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-17 17:01:39 +0900

    不要なコードのコメントアウト（高森）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index]
   before_action :authenticate_user!, only: :new
   def index
-    @item = Item.all
+    #@item = Item.all
     #@item = Item.order("created_at DESC")
   end
   
