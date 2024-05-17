f2169ccf3a691177fcf96285ca9876c666a13822
watanabetomoya0727 <watanabe.tomoya@laiblitz.co.jp>
2024-05-17 18:36:01 +0900

    コメントアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   def index
     @item = Item.all
-
-    #@item = Item.order("created_at DESC")
+    @item = Item.order("created_at DESC")
 
   end
   
