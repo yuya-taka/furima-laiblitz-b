7998f95c046e0094baf3a63bebc8801614a14765
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-20 18:59:29 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   # サインインしていないとき、インデックスページしか見れない機能
   def move_to_index
     unless user_signed_in? 
-      redirect_to action: :index
+      redirect_to new_user_session_path
     end
   end
 
