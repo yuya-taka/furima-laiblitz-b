95a080b5199575a82d79bd571a5ea7c85dfe7902
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-20 14:55:41 +0900

    【修正】不要箇所の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index]
   before_action :authenticate_user!, only: :new
   def index
-    @items = Item.all
     @items = Item.order("created_at DESC")
   end
   
