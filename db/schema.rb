7fdfadc59f8fb3e6886d9482e02b99660f1d5cd4
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-20 12:44:01 +0900

    売れているときのみsold out表示と、商品がないときのみダミー商品を配置

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_17_065253) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_20_032650) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_065253) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "histories", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_histories_on_item_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
@@ -74,5 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_065253) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "histories", "items"
+  add_foreign_key "histories", "users"
   add_foreign_key "items", "users"
 end
