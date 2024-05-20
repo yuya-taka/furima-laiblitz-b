b336f292030a61227e270780a36477c29a805e2d
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-20 16:57:03 +0900

    【９】商品を編集し、保存できるようにしました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
 
