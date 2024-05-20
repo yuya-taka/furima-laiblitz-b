b36a23946e6b5df0d842dbdf8d6b90c865303f02
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-20 18:20:49 +0900

    ミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
   def update
     # item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
     end
