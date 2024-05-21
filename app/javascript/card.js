37a18f3e29c62ed380b2d2be2dce31112c828856
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-21 12:00:05 +0900

    公開鍵設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,5 @@
 const pay = () => {
+  const payjp = Payjp('sk_test_2688513c0dda39e086c8ecfe')// PAY.JPテスト公開鍵
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
