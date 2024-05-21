ff0db15e4c50f75638ede6b4b0e17b7ae55a19ec
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-21 16:25:27 +0900

    クレカ決済機能の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-
   resources :items
+  resources :buyers, only:[:index, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_20_032650) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_21_071113) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,13 +39,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_20_032650) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "histories", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
+  create_table "buyers", charset: "utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "street_address", null: false
+    t.string "building", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_histories_on_item_id"
-    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
@@ -83,7 +84,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_20_032650) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "histories", "items"
-  add_foreign_key "histories", "users"
   add_foreign_key "items", "users"
 end
