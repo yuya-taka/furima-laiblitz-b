2852900d26b611aa145588ea3ab900208b832c98
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-21 12:16:38 +0900

    フォームにクレジットカード情報の入力欄を追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,13 @@
 const pay = () => {
   const payjp = Payjp('sk_test_2688513c0dda39e086c8ecfe')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
