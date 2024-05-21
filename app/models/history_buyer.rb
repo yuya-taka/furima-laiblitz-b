0dab67a2e0ffd0b87ddc2cdad0e3da9c3d92d642
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-21 16:20:08 +0900

    【11】Formモデルの作成

---
+++ app/models/history_buyer.rb
@@ -0,0 +1,12 @@
+class HistoryBuyer
+  include ActiveModel::Model
+  attr_accessor :item, :user, :post_code, :prefecture_id, :city, 
+                :street_address, :building, :phone_number, :history
+
+  with_options presence: true do
+    validates :item, :user, :post_code, :city, :street_address, 
+              :building, :phone_number, :history
+    #validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+end
