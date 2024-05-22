d488fda435cc749d3481e77c40f768e39c1058ce
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-22 14:13:04 +0900

    トークンの値をフォームに追加し、クレカ情報の非表示

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,8 +14,14 @@ const pay = () => {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
     e.preventDefault();
   });
