ad5a91792c4c3e8e0b559bbb7e948dc08d3f3bc7
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-22 17:06:00 +0900

    バリデーション

--- app/models/history_buyer.rb
+++ app/models/history_buyer.rb
@@ -2,16 +2,20 @@ class HistoryBuyer
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, 
                 :street_address, :building, :phone_number, :history_id
-
+  validates :token, presence: true
   with_options presence: true do 
-    validates :item_id, :user_id, :post_code, :city, 
-              :street_address, :phone_number
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :item_id, :user_id, :post_code
   end
+
+  validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "は「3桁ハイフン4桁」の形式で入力してください" }
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  with_options presence: true do 
+    validates :city, :street_address, :phone_number
+  end
+
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁以上11桁以内の半角数値のみ入力してください" }
 
   attr_accessor :token
-  validates :token, presence: true
 
   def save
     # 購入履歴を保存する
