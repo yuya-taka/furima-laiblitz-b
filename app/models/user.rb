196d40dd0a457ea912e3e0961d92b3e53986ca7e
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-24 12:09:03 +0900

    フォローしているかを判定するメソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  #  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効なメールアドレスではありません" }
+
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
 
   validates :family_name, :first_name, presence: true,
@@ -16,9 +14,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :histories
 
+
+  #フォロー機能実装のためのアソシエーションとフォロー判定
+
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
 
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    follower =  passive_relationships.find_by(following_id: user.id)
+    return follower.present?
+  end
 end
