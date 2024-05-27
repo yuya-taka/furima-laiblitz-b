a25152e80a77e78af4851f47bfeec8a9db4b4aee
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-27 12:24:27 +0900

    コメント機能のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders
     resource :likes, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   resources :users do
     resource :relationships, only: [:create, :destroy]
