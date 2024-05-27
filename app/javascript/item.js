a6c13c4e8da617a9916580b081e731ea7488579a
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-27 18:27:39 +0900

    不要な記述を削除し販売利益と販売手数料を表示

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,9 +1,6 @@
 
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-    // 新規投稿・編集ページのフォームを取得
-    const itemForm = document.querySelector('hipping-address-form');
-    if (!itemForm) return null;
     priceInput.addEventListener("input", () => {
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
