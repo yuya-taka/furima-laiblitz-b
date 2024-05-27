6387bf0b46a3d95b0870ac6238f2b696e6bcb41e
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-27 10:25:54 +0900

    ログインしていない場合のいいね非表示

--- db/schema.rb
+++ db/schema.rb
@@ -95,10 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_24_032055) do
     t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
+  create_table "user_profiles", charset: "utf8", force: :cascade do |t|
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
@@ -132,4 +129,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_24_032055) do
   add_foreign_key "user_profiles", "users"
   add_foreign_key "likes", "items"
   add_foreign_key "likes", "users"
+  add_foreign_key "user_profiles", "users"
 end
