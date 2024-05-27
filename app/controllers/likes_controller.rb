136799e56b3f75f88ba7edda5f528daf813a85a7
tsuchishota <tsuchiya.shotarou@laiblitz.co.jp>
2024-05-27 10:58:58 +0900

    いいね機能の非同期通信

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,24 @@
 class LikesController < ApplicationController
+  before_action  :set_item
 
   def create
     like = current_user.likes.build(item_id: params[:item_id])
     like.save
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(item_id: params[:item_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  
 end
