ef8ad5792db8d662cf7f84647736742f2b083cc0
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-05-28 17:28:00 +0900

    【追】カテゴリー追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,17 +1,20 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' }, # ←こちらに注目しましょう
+    { id: 1, name: '---' },
     { id: 2, name: 'レディース' },
     { id: 3, name: 'メンズ' },
     { id: 4, name: 'ベビー・キッズ' },
     { id: 5, name: 'インテリア・住まい・小物' },
     { id: 6, name: '本・音楽・ゲーム' },
     { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' },
-    { id: 11, name: 'その他' }
-  ]
+    { id: 8, name: 'コスメ・香水・美容' },
+    { id: 9, name: '家電・スマホ・カメラ' },
+    { id: 10, name: 'スポーツ・レジャー' },
+    { id: 11, name: 'ハンドメイド' },
+    { id: 12, name: 'チケット' },
+    { id: 13, name: '自動車・オートバイ' },
+    { id: 14, name: 'その他' }
+]
 
   include ActiveHash::Associations
   has_many :items
